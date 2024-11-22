0963630c071e57e7d67d080bf4c653b3888c22be
invent365 <93697587+invent365@users.noreply.github.com>
2024-11-22 01:20:00 -0800

    Update Transformer_block.py

--- Transformer_block.py
+++ Transformer_block.py
@@ -12,6 +12,9 @@ embedding1 = nn.Embedding(base_model_encoding.n_vocab, embed_dim)
 class Transformer_Block(nn.Module):
     def __init__(self, embed_dim:int, num_heads:int, num_parameters:int, num_queries_per_group:int,dk=embed_dim,dv=embed_dim):
         super(Transformer_Block,self).__init__()
+        """
+        Typical rendering of a transformer model that uses grouped query attention instead of multi-head attention.
+        """
         self.embed_dim = embed_dim
         self.num_heads = num_heads
         self.dk,self.dv = dk,dv
@@ -20,10 +23,11 @@ class Transformer_Block(nn.Module):
         self.ffn = basics.Position_Feedforward(self.embed_dim,num_parameters)
         #self.res_block = basics.Residual_Block(self.embed_dim, length=length_of_res_block)
     def forward(self,x:torch.Tensor):
-        normed_input = self.rms(x)
-        out = self.transformer_multihead(normed_input)
+        normed_input = self.rms(x) #compute the root mean square norm to even inputs
+        out = self.transformer_multihead(normed_input) #pass through the multihead
         out = x + out # add_norm_1
-        normed_output2 = self.rms(out)
+        normed_output2 = self.rms(out) #compute root mean square to even out the outputs. This ensures that sufficient patterns can be extracted from the data that can be propagated across the model.
+        #Doing so adds to the "intelligence" of the model
         normed_output2 = self.ffn(normed_output2)
         out = out + normed_output2
         return out
